97d2ca8481afed34856976b84ef0a9fa91ea04bd
SHIVSSV1269 <shivaselvamssv@gmail.com>
2025-07-26 14:09:02 +0530

    Add files via upload

---
+++ DAY1/BandGenerator.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator")
+city=input("What's the city you grew up in?\n")
+pet=input("What is your name of the pet?\n")
+print("Your Band Name could be: " + city +" " + pet)
